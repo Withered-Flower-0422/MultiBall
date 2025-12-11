8d101344be3550f2e5c129fffa444ed6caa6de87
withered_flower <2734850178@qq.com>
2025-12-12 00:33:15 +0800

    modify some files

--- Assets/Scripts/MultiBall/MultiBallTip.js
+++ Assets/Scripts/MultiBall/MultiBallTip.js
@@ -2,7 +2,7 @@ import { settings, levelManager } from "gameApi";
 let tipGuid;
 let activated = true;
 let sectionFinished = false;
-const multiBallManager = {};
+let multiBallManager;
 const mouseButtons = new Set(["Left", "Middle", "Right"]);
 export const init = (self, v) => Object.assign(globalThis, v);
 export const registerEvents = [
@@ -18,7 +18,7 @@ export const onEvents = (self, events) => {
         const msg = events.OnReceiveCustomEvent[0];
         if (isMultiBallMessage(msg)) {
             if (msg.OnLoadMultiBall) {
-                Object.setPrototypeOf(multiBallManager, msg.OnLoadMultiBall.multiBallManager);
+                multiBallManager = msg.OnLoadMultiBall.multiBallManager;
             }
             if (msg.OnPostMultiBallAppendEnd) {
                 if (!activated)
