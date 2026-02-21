6f66c48899feb8d59176bfff35ca2c3a1ea0d406
withered_flower <2734850178@qq.com>
2026-02-21 23:36:00 +0800

    update Manager.d.ts

--- Assets/Scripts/UtilClass/Manager.d.ts
+++ Assets/Scripts/UtilClass/Manager.d.ts
@@ -1,24 +1,24 @@
 import { settings } from "gameApi";
-import type { Events } from "game:alias";
-export default abstract class Manager<Event extends {
+import type { Events as E } from "game:alias";
+export default abstract class Manager<Events extends {
     _brand: symbol;
 } = {
     _brand: symbol;
 }, TipKey extends string = never> {
     protected readonly eventSymbol: symbol;
-    protected canceledEvents: Set<keyof Event & `OnPre${string}`>;
-    protected sendEvent<T extends Exclude<keyof Event, "_brand">>(name: T, data: Event[T]): void;
+    protected canceledEvents: Set<keyof Events & `OnPre${string}`>;
+    protected sendEvent<T extends Exclude<keyof Events, "_brand">>(name: T, data: Events[T]): void;
     /**
      * Checks whether the given object is an event of this manager.
      * @param e - The object to check.
      * @returns Whether the object is an event of this manager.
      */
-    isSelfEvent(e: any): e is Event;
+    isSelfEvent(e: any): e is Events;
     /**
      * Cancels the given event.
      * @param event - The event to cancel.
      */
-    cancelEvent(event: keyof Event & `OnPre${string}`): void;
+    cancelEvent(event: keyof Events & `OnPre${string}`): void;
     protected abstract get tipText(): Record<TipKey, Record<typeof settings.language, string>>;
     /**
      * Shows the tip with the given key for the given duration.
@@ -35,5 +35,5 @@ export default abstract class Manager<Event extends {
      * Updates the manager.
      * @param e - The event data.
      */
-    abstract update(e: Events): void;
+    abstract update(e: E): void;
 }
